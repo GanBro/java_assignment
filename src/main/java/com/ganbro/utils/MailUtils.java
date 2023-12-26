e48ad5730b0e0886451fa406968d7aea2b71f9f4
wubo <2551921037@qq.com>
2023-12-27 01:05:47 +0800

    修改邮件标题

--- src/main/java/com/ganbro/utils/MailUtils.java
+++ src/main/java/com/ganbro/utils/MailUtils.java
@@ -47,7 +47,7 @@ public class MailUtils {
         InternetAddress to = new InternetAddress(email);
         message.setRecipient(RecipientType.TO, to);
         // 设置邮件标题
-        message.setSubject("图书馆管理系统");
+        message.setSubject("吴波的图书馆管理系统");
         // 设置邮件的内容体
         message.setContent("尊敬的用户:你好!\n注册验证码为:" + code + "(有效期为一分钟,请勿告知他人)", "text/html;charset=UTF-8");
         // 最后当然就是发送邮件啦
